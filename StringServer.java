d599d70b8c0fc07959b356fece334de34f4d031d
TimothyLam727 <146874935+TimothyLam727@users.noreply.github.com>
2023-10-17 11:33:43 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,1 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
 
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/add-message")) {
+            String output[] = url.getQuery().split("=");
+            return String.format(num, ". ", output[1]);
+        } 
+            return "404 Not Found!";
+    }
+}
+
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
